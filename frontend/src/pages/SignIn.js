c36d3910db15c7a12456d34241d36845ffff2f00
chaehyeong <103627328+chaehyeong@users.noreply.github.com>
2026-09-15 01:56:00 +0900

    SignIn complete

--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -4,7 +4,7 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import './SignIn.css'
 import logo1 from '../assets/logo1.png';
-import logo2 from '../assets/logo2.png';
+import logo3 from '../assets/logo3.png';
 
 function SignIn() {
     useEffect(() => {
@@ -15,13 +15,13 @@ function SignIn() {
     }, []);
 
     return (
-        <div>
+        <div className='body'>
             <div className='signin'>
                 <div className='sign-logo'>
                     <img src={logo1} alt='logo1'/>
-                    <img src={logo2} alt='logo2'/>
+                    <img src={logo3} alt='logo2'/>
                 </div>
-                <div className='form'>
+                <div className='form1'>
                     <Form>
                         <Form.Group className="mb-3" controlId="formGroupEmail">
                             <Form.Label>Email address</Form.Label>
@@ -35,17 +35,22 @@ function SignIn() {
                     <div className="d-grid gap-2">
                         <Button id='login-btn'><NavLink className='char' to='/HomePage'>로그인</NavLink></Button>
                     </div>
-                    <NavLink className='fake-btn' to='#'>비밀번호 찾기</NavLink>
+                    <div className='option'>
+                        <NavLink className='option1' to='#'>비밀번호 찾기</NavLink>
+                        <span> | </span>
+                        <NavLink className='option2' to='/SignUp'>회원가입</NavLink>
+                    </div>
                 </div>
                 <div className='line'>
                     <div></div>
                     <span className='text'>또는</span>
                     <div></div>
                 </div>
-                {/*
-                지금은 걍 로그인 누르면 바로 홈화면 넘어가는 상태니까 
-                백엔드쪽에서 알아서 잘 하시리라 믿고 나머지는 그냥 갑자기 의욕 떨어져서 못하겟고 자야겟다
-                */}
+                <div className='form2'>
+                    <div className="d-grid gap-2">
+                        <Button id='info21-btn'><a href='https://info21.khu.ac.kr/com/LoginCtr/login.do?sso=ok'>경희대학교 통합 로그인</a></Button>
+                    </div>
+                </div>
             </div>
         </div>
     );
